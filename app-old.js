81827d71b1421ba2563c4c20e125a1c4078661f6
Maycol <maycol320@gmail.com>
2021-01-29 10:02:03 -0300

    Primer Commit

---
+++ app-old.js
@@ -0,0 +1,22 @@
+const http = require('http');
+
+http.createServer((req, rest) => {
+
+        res.writeHead(200, { 'Content-Type': 'application/json' });
+
+        let salida = {
+            nombre: 'Maycol',
+            edad: 26,
+            url: req.url
+
+        }
+
+        res.write(JSON.stringify(salida));
+
+        // res.write('Hola mundo');
+        res.end();
+
+    })
+    .listen(8080);
+
+console.log('escuchando el puerto 8080');
